dd36fb7cc8af815525e3f8ec6c2c063c72344563
Dmitlaw <dmitpechagin@yandex.ru>
2025-10-27 23:24:37 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,98 @@
+#include <fstream>
+#include <iostream>
+
+class Address {
+private:
+    std::string city;
+    std::string street;
+    int house_number;
+    int flat;
+
+public:
+    Address() = delete;
+    Address(std::string city, std::string street, int house_number, int flat) {
+        this->city = city;
+        this->street = street;
+        this->house_number = house_number;
+        this->flat = flat;
+    }
+
+    std::string getCity () {
+        return this->city;
+    }
+    std::string  getStreet() {
+        return this->street;
+    }
+    int getHouse() {
+        return this->house_number;
+    }
+    int getFlat() {
+        return this->flat;
+    }
+};
+
+int main() {
+
+    int n = 0;
+    Address** arr = new Address*[n];
+
+    std::ifstream fin("in.txt");
+    if (!fin) {
+        std::cerr << "Не удалось открыть файл in.txt";
+        return 1;
+    }
+
+    fin >> n;
+    fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    std::cout << "Текст в файле in.txt: " << std::endl;
+
+    for (int i = 0; i < n; ++i) {
+        std::string city;
+        std::string street;
+        int house_number;
+        int flat;
+
+        fin >> city >> street >> house_number >> flat;
+        arr[i] = new Address(city, street, house_number, flat);
+
+        std::cout << arr[i]->getCity();
+        std::cout << " ";
+        std::cout << arr[i]->getStreet();
+        std::cout << " ";
+        std::cout << arr[i]->getHouse();
+        std::cout << " ";
+        std::cout << arr[i]->getFlat();
+        std::cout << std::endl;
+    }
+    fin.close();
+
+    std::stable_sort(arr, arr + n, [](Address* lhs, Address* rhs) {
+    return lhs->getCity() < rhs->getCity();
+});
+
+    std::ofstream out("out.txt");
+    if (!out) {
+        std::cerr << "Не удалось открыть out.txt для записи\n";
+        return 1;
+    }
+
+    out << n << '\n';
+    for (size_t i = 0; i < n; ++i) {
+        out << arr[i]->getCity() << ", "
+            << arr[i]->getStreet() << ", "
+            << arr[i]->getHouse() << ", "
+            << arr[i]->getFlat()  << '\n';
+    }
+
+    out.close();
+
+    std::ifstream fin_out("out.txt");
+
+    std::cout << std::endl;
+    std::cout << "Текст в файле out.txt: " << std::endl;
+    std::cout << fin_out.rdbuf();
+    fin_out.close();
+
+    return EXIT_SUCCESS;
+}
